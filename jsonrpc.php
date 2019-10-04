cd4cd2c333e991d22789fdc0d1c8c21b2b0da53d
cakyus <cakyus@gmail.com>
2019-10-04 08:59:06 +0700

    use config.php and functions.php

--- jsonrpc.php
+++ jsonrpc.php
@@ -1,7 +1,25 @@
 <?php
+
+/**
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2
+ * as published by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ **/
+
+include('config.php');
+
 /**
  * JSON RPC
- * 
+ *
  * @link https://github.com/cakyus/jsonrpc
  **/
 
@@ -77,6 +95,9 @@ class JsonRpcServer {
 		
 		$this->id = $request->id;
 		
+
+		include('functions.php');
+
 		if (function_exists($request->method)) {
 			return $this->getResponseResult(
 				call_user_func_array($request->method, $request->params)
